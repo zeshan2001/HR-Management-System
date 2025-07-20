c4bba589122a54a7de916e92cf0711940105af49
alqaassass <ali.alqassas02@gmail.com>
2025-07-20 13:10:37 +0300

    added all the models

--- models/project.js
+++ models/project.js
@@ -1,11 +1,27 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-// const projectSchema = new mongoose.Schema(
-//   {
-//     proName:{}
-//   },
-//   { timestamps: true }
-// )
-// const Project = mongoose.model('Project', userSchema)
+const projectSchema = new mongoose.Schema(
+  {
+    projectName:{ type: String, required: true},
+    numberOfEmployees:{ type: Number, required: true},
+    startDate:{ type: Date, required: true},
+    deadLine:{ type: Date, required: true},
+    budget:{ type: Number, required: true},
+    employeesInProject:{
+        type: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                 ref: 'User'
+                }
+        ],
+    },
+    departmentOfProject: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+  },
+  { timestamps: true }
+)
+const Project = mongoose.model('Project', projectSchema)
 
-// module.exports = Project
+module.exports = Project
