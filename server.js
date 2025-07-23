6aecf6340c09ed234faed554e8b85d0d9acb851b
Ham33d-754 <mahmoodish873@gmail.com>
2025-07-23 11:29:54 +0300

    added css to the nav bar

--- server.js
+++ server.js
@@ -12,30 +12,10 @@ const isSignedIn = require('./middlewares/is-signed-in')
 const MongoStore = require('connect-mongo')
 
 const app = express()
+app.use(express.static('public'))
 
 const PORT = process.env.PORT ? process.env.PORT : 3000
 const database = require('./config/db')
-
-
-const multer  = require('multer')
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '/tmp/my-uploads')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
-  }
-})
-
-const upload = multer({ dest: 'uploads/' })
-
-app.post('/stats', upload.single('uploaded_file'), function (req, res) {
-  // req.file is the name of your file in the form above, here 'uploaded_file'
-  // req.body will hold the text fields, if there were any
-  console.log(req.file, req.body)
-});
 
 // Middlewares
 app.use(express.urlencoded({ extended: false }))
